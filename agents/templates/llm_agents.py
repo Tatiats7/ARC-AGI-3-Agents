47e59656dd7ced885369a7e84883cb146b950a5b
Tatia Tsmindashvili <tatiatsmindashvili@Tatias-MacBook-Air-2.local>
2026-02-12 23:30:44 +0400

    log additional data ability

--- agents/templates/llm_agents.py
+++ agents/templates/llm_agents.py
@@ -396,6 +396,8 @@ Call exactly one action.
                         self.frames[-1]
                     ),
                 }
+                if "langgraph_meta" in kwargs:
+                    meta["langgraph_meta"] = kwargs["langgraph_meta"]
                 self.recorder.record(meta)
         super().cleanup(*args, **kwargs)
 
